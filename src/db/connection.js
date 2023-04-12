110d4a74b5086bbf60017e071f8edd993641d3ac
AllisterF1 <allytheforbes@gmail.com>
2023-04-12 14:33:28 +0100

    add db connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("db connected");
   } catch (error) {
     console.log(error);
   }
