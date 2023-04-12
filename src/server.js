e7dfca09831bae2dd0d86bd16861c9f73a6688a5
AllisterF1 <allytheforbes@gmail.com>
2023-04-12 16:18:19 +0100

    add get all books

--- src/server.js
+++ src/server.js
@@ -1,15 +1,46 @@
 require("dotenv").config();
 require("./db/connection");
 
-
 const express = require("express");
+
+const Book = require("./books/model");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/books", (req, res) => {
-    res.send("Hello world");
+app.get("/books/getallbooks", async (req, res) => {
+  try {
+    const books = await Book.find({});
+
+    const successResponse = {
+      message: "success",
+      books: books,
+    };
+    res.status(200).json(successResponse);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/books/addbooks", async (req, res) => {
+  try {
+    //const newBook = await Book.create(req.body); this would work also
+    const newBook = await Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+    });
+
+    const successResponse = {
+      message: "success",
+      newBook: newBook,
+    };
+
+    res.status(201).json(successResponse);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(5001, () => console.log("server is listening"));
